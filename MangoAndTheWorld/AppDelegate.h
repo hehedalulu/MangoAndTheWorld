6e29da10b6bc1efac75bc8a791482c9be13a35f1
王璐璐 <982472770@qq.com>
2017-06-01 19:14:16 +0800

    Initial Commit

---
+++ MangoAndTheWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MangoAndTheWorld
+//
+//  Created by Wll on 17/6/1.
+//  Copyright © 2017年 CherryWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MangoAndTheWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MangoAndTheWorld
+//
+//  Created by Wll on 17/6/1.
+//  Copyright © 2017年 CherryWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
